eb0ec160bd79e56fbe8aaa814db1229a2efd55e4
yeojin Kim <jjiny2022@kakao.com>
2022-07-26 00:17:23 +0900

    220725

---
+++ src/components/pages/Slider.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../layout/Header';
+import Contents from '../layout/Contents';
+import SliderCont from '../includes/SliderCont';
+
+function Slider() {
+  return (
+    <>
+    <Header/>
+      <Contents>
+         <SliderCont/>
+      </Contents>
+
+    </>
+  );
+}
+
+export default Slider;
